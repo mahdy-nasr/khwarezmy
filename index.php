0e23156d2efce48504d76470715f461f31ae2731
mahdy-nasr <mahdy.m.nasr@gmail.com>
2016-10-27 09:43:33 +0300

    asd

--- index.php
+++ index.php
@@ -102,13 +102,24 @@ Author URL: http://khwarezmy.com
 
 </head>
 <body>
+	<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-86402117-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
 <!--Start of Tawk.to Script-->
 <script type="text/javascript">
 var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
 s1.async=true;
-s1.src='https://embed.tawk.to/580c5ff0a42095204693c1b8/default';
+s1.src='http://embed.tawk.to/580c5ff0a42095204693c1b8/default';
 s1.charset='UTF-8';
 s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
